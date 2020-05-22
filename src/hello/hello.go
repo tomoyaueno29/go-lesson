4cf26f2fbddf91407892b430456900c79e3f9d46
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-22 16:26:12 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 
 func main() {
-	var x int = 1
-	xx := float64(x)
-	fmt.Printf("%T %v %f\n", xx, xx, xx)
-
-	var s string = "14"
-	i, _ := strconv.Atoi(s)
-	fmt.Printf("%T %v\n", i ,i)
+	
 	
 }
