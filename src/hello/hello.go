a73b654fe514938759e39e4d8635dc3ac19a9e6e
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-22 16:54:02 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 )
 
-func add(){
-	xd
+func add(x int, y int) (int, int) {
+	return x + y, x - y
+}
+
+
+func cal(price int, item int) (int) {
+
+	result := price * item
+	return result
 }
 
 func main() {
-	
+	result, result2 := add(20, 10)
+	fmt.Println(result, result2)
+
+	r3 := cal(100, 2)
+	fmt.Println(r3)
 }
