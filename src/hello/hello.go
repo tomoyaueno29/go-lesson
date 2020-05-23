17719727bd497a14309fb010e9d91260db3044d5
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-23 23:16:30 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,27 +4,12 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) (int, int) {
-	return x + y, x - y
-}
-
-
-func cal(price int, item int) (result int) {
-
-	result = price * item
-	return
-}
-
 func main() {
-	result, result2 := add(20, 10)
-	fmt.Println(result, result2)
 
-	r3 := cal(100, 2)
-	fmt.Println(r3)
+	var i int = 1
+	var t bool = true
 
-	f := func(x int) {
-		fmt.Println("aaa")
-		fmt.Print(x)
-	}
-	f(100)
+	fmt.Println(i, t)
+
+	
 }
