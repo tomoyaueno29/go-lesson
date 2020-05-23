8b02cd3566ecd96d6d1f3cf50cd9aac7d5b6195a
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-23 23:46:57 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,7 +6,21 @@ import (
 
 const Pi = 3.14
 
+ var big int = 98374645128
+
 func main() {
-	
-	fmt.Println(xi, xf64, xf32 , xs, xt, xf)
+
+	// var(
+	// 	u8 uint       = 255
+	// 	i8 int8       = 127
+	// 	f32 float32   = 0.2
+	// 	c64 complex64 = -5 + 12i
+	// )
+	// fmt.Println(u8, i8, f32, c64)
+
+	// fmt.Printf("%T\n%v\n", u8, u8)
+
+	x := 0
+	fmt.Println(x)
+	x++
 }
