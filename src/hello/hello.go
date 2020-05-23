a2dc0c6e9b0616b8f928d00d1d50b37dd1e4d93e
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-23 23:59:43 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-const Pi = 3.14
-
- var big int = 98374645128
 
 func main() {
 
-	fmt.Println("Hello World")
-	fmt.Println(string("Hello World"[0]))
-
-	var s string = "Hello World"
-	s = strings.Replace(s, "H", "X", 1)
-	fmt.Println(s)
-
+	t, f := true, false
+	fmt.Printf("%T %v", t, t)
+	fmt.Printf("%T %v", f, f)
 }
