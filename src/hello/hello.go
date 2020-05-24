b9fa88952805bcb6f94a3e75795ecc3638056e7c
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-24 11:57:37 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,7 +8,18 @@ import (
 
 func main() {
 
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d", len(n))
+	num := 11
+	if num % 2 == 0 {
+		fmt.Println("by 2")
+	}else if num%3==0{
+		fmt.Println("by 3")
+	}else{
+		fmt.Println("else")
+	}
+
+	x, y := 10, 10
+	if x == 10 && y == 10{
+		
+	}
 	
 }
