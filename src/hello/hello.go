3921333ebeeb6d93096342c88f438f88ca1c37ca
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-25 11:36:23 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"os"
-	"log"
+	
 	"fmt"
 )
 
+
+
 func main() {
-	 file, err := os.Open("./lesson.go")
-
-	 if err != nil {
-		 log.Fatalln("EXIT", err)
-	 }
-	 defer file.Close()
-	 data := make([]byte, 100)
-
-	 count, err := file.Read(data)
-
-	 if err != nil{
-		 log.Fatalln("error")
-	 }
-	 fmt.Println(count, string(data))
+	 
 }
