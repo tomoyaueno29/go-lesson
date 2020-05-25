b55af5b36f261af86babdf1eaf24bf74db58c2b7
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-25 12:31:29 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -10,8 +10,16 @@ type Vertex struct{
 	Z string
 }
 
-func main() {
+func changeVertex(v *Vertex){
+	(*v).X = 1000
+}
 
+func main() {
+	v := Vertex{1,2, "test"}
+	changeVertex(&v)
+	fmt.Println(v)
+
+	/*
 	v := Vertex{X: 1, Y: 2, Z: "aaa"}
 	fmt.Println(v)
 	fmt.Println(v.X)
@@ -21,4 +29,5 @@ func main() {
 
 	var v5 Vertex
 	fmt.Println(v5)
+	*/
 }
