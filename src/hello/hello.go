554a4b08580fbc16fc24af8575cb9c4f69761f68
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-25 12:08:15 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	
 	"fmt"
 )
 
-
+func one(x *int){
+	*x = 50000
+	fmt.Println(*x)
+}
 
 func main() {
-	 
+	var x int = 100
+	fmt.Println(x)
+	one(&x)
+
+	fmt.Println(x)
+	
 }
