1fac55d9300f07388c47a0500cb5a564bd432924
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 13:36:56 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,22 +8,9 @@ type Vertex struct{
     X, Y int
 }
 
-func (v Vertex) Plus() int{
-    return v.X + v.Y
-}
 
-
-func (v *Vertex) Scale(i int){
-    v.X = v.X * i
-    v.Y = v.Y * i
-}
-
-// func Plus(v Vertex) int {
-//     return v.X + v.Y
-// }
  
 func main(){
     v := Vertex{3, 4}
-    // fmt.Println(Plus(v))
-    fmt.Println(v.Plus())
+    fmt.Println(v)
 }
