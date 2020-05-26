ecc29b3394bf00b3db418781c319ef16fbac85fd
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 15:01:36 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,22 +4,6 @@ import (
     "fmt"
 )
 
-func  goroutine1(s []int, c chan int){
-
-    sum := 0
-    for _, v := range s {
-        sum += v
-        c <- sum
-    }
-    close(c)
-}
-
 func main(){
-    s := []int{1,2,3,4,5}
-    c := make(chan int)
-    go goroutine1(s, c)
     
-    for i := range c{
-        fmt.Println(i)
-    }
 }
