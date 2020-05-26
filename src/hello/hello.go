26633f515e40b2e486cf7f5637eda8f07657cf5f
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 14:04:18 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,13 +4,20 @@ import (
     "fmt"
 )
 
+type User struct {
+    gender string
+    age int
 
+}
+
+func (u User) String() string{
+    return fmt.Sprintf("My name is %v\n", u.gender)
+}
 
 func main(){
    
-    s := Student{"Tomoya", 60.8, 90.0}
-    fmt.Println(s.Name)
-    fmt.Println(s.Math)
-    fmt.Println(s.English)
-    fmt.Println(s.Plus())
+    u := User{gender: "male", age: 22}
+    fmt.Println(u.gender)
+    fmt.Println(u.age)
+    fmt.Println(u.String())
 }
