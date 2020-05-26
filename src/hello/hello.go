8e9bef50b7ea0b72a7ee8f2683d0e1430289cfde
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 12:31:13 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,42 +4,15 @@ import (
 	"fmt"
 )
 
-type Vertex struct{
-    x, y int
-}
+type MyInt int
 
-func (v Vertex) Area() int{
-    return v.x * v.y
-}
+func (i MyInt) Double() int {
+    fmt.Printf("%T %v\n", i, i)
 
-func (v *Vertex) Scale(i int) {
-    v.x = v.x * i
-    v.y = v.y * i
-}
-
-type Vertex3D struct{
-   Vertex
-   z int
-}
-
-func (v Vertex3D) Area3D() int{
-    return v.x * v.y * v.z
-}
-
-func (v *Vertex3D) Scale3D(i int) {
-    v.x = v.x * i
-    v.y = v.y * i
-    v.z = v.z * i
-}
-
-func New(x, y, z int) *Vertex3D {
-    return &Vertex3D{Vertex{x,y}, z}
+    return int(i * 2)
 }
 
 func main() {
-
-   v := New(3, 4, 5)
-   v.Scale3D(10)
-//    fmt.Println(v.Area())
-   fmt.Println(v.Area3D())
+    myInt := MyInt(10)
+    fmt.Println(myInt.Double())
 }
