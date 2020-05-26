9e2bfd149c3f82dd8a737603c81b012d87bbb015
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 13:04:35 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,26 +4,17 @@ import (
 	"fmt"
 )
 
-func do(i interface{}){
-    // ii := i.(int)
-    // ii *= 2
-    // fmt.Println(ii)
-    // ss := i.(string)
-    // fmt.Println(ss)
+type Person struct {
+    Name string
+    Age int
+}
 
-    switch v := i.(type) {
-    case int:
-        fmt.Println(v * 2)
-    case string:
-        fmt.Println(v + "!")
-    default:
-        fmt.Printf("I don't know %T\n", v)
-    }
+func (p Person) String() string {
+    return fmt.Sprintf("My name is %v", p.Name)
 }
 
 func main() {
     // var i interface{} = 10
-    do(10)
-    do("Mike")
-    do(true)
+    mike := Person{"Mike", 22}
+    fmt.Println(mike)
 }
