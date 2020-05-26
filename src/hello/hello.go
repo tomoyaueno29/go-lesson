beecae59e3f094e9492a64f7db5df96b791bebf0
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 12:03:35 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,15 +4,12 @@ import (
 	"fmt"
 )
 
+type Vertex struct{
+    
+}
+
 func main() {
-    var i int = 100
-    var j int = 200
-    var p1 *int
-    var p2 *int
-    p1 = &i
-    p2 = &j
-    i = *p1 + *p2
-    p2 = p1
-    j = *p2 + i
-    fmt.Println(j)
+   
+
+
 }
