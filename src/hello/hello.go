4eace12f10c8bb9eee429dde14680db2b42ba087
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 12:23:48 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -17,18 +17,29 @@ func (v *Vertex) Scale(i int) {
     v.y = v.y * i
 }
 
-// func Area(v Vertex) int {
-//     return v.x * v.y
-// }
+type Vertex3D struct{
+   Vertex
+   z int
+}
 
-func New(x, y int) *Vertex {
-    return &Vertex{x, y}
+func (v Vertex3D) Area3D() int{
+    return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+    v.x = v.x * i
+    v.y = v.y * i
+    v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+    return &Vertex3D{Vertex{x,y}, z}
 }
 
 func main() {
-//    v := Vertex{3, 4}
-//    fmt.Println(Area(v))
-   v := New(3, 4)
-   v.Scale(10)
-   fmt.Println(v.Area())
+
+   v := New(3, 4, 5)
+   v.Scale3D(10)
+//    fmt.Println(v.Area())
+   fmt.Println(v.Area3D())
 }
