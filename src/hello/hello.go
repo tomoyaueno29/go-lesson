9efa8b15e8ac3b960fca109bd7e6b37fc66d2e1d
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 12:19:56 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,26 +5,30 @@ import (
 )
 
 type Vertex struct{
-    X, Y int
+    x, y int
 }
 
 func (v Vertex) Area() int{
-    return v.X * v.Y
+    return v.x * v.y
 }
 
 func (v *Vertex) Scale(i int) {
-    v.X = v.X * i
-    v.Y = v.Y * i
+    v.x = v.x * i
+    v.y = v.y * i
 }
 
-func Area(v Vertex) int {
-    return v.X * v.Y
+// func Area(v Vertex) int {
+//     return v.x * v.y
+// }
+
+func New(x, y int) *Vertex {
+    return &Vertex{x, y}
 }
 
 func main() {
-   v := Vertex{3, 4}
-   fmt.Println(Area(v))
-   v.Area()
+//    v := Vertex{3, 4}
+//    fmt.Println(Area(v))
+   v := New(3, 4)
    v.Scale(10)
    fmt.Println(v.Area())
 }
