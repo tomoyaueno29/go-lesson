1e0beeb19d4d8194842bab4ee2a770ec4e8d9b35
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 15:40:32 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -10,23 +10,19 @@ func main(){
     
     tick := time.Tick(100 * time.Millisecond)
     boom := time.After(500 * time.Millisecond)
-
+    OuterLoop2:
     for {
-        isBreak := true
         select {
 
         case t := <- tick:
             fmt.Println("tick.", t)
         case <-boom:
             fmt.Println("BOOM!")
-            // break
+            break OuterLoop2
             // return 
         default:
             fmt.Println("    .")
             time.Sleep(50 * time.Millisecond)
-        }
-        if isBreak {
-            break
         }
         
     }
