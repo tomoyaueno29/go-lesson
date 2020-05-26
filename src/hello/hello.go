6783f43379e1859eefb1268690c389fd883536bd
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 15:04:56 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,6 +4,17 @@ import (
     "fmt"
 )
 
+func producer(ch chan int, i int){
+    // Something
+    ch <- i * 2
+}
+
 func main(){
-    
+    var wg sync.WaitGroup
+    ch := make(chan int)
+
+    for i := 0; i<10; i++{
+        wg.Add(1)
+        go producer(ch, i)
+    }
 }
