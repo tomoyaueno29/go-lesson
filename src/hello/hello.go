ca3b78b9064ea424a8cea406781c8a48b95b89a5
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-26 15:24:45 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,42 +2,30 @@ package main
  
 import (
     "fmt"
+    "time"
 )
 
-func producer(first chan int){
-    defer close(first)
-    for i := 0; i<10; i++{
-        first <- i
+func goroutine1(ch chan string){
+
+    for {
+        ch <- "packet from 1"
+        time.Sleep(1 * time.Second)
     }
 }
 
-func multi2(first <-chan int, second chan<- int){
-    defer close(second)
-    for i := range first{
-        second <- i * 2
+func goroutine2(ch chan string){
+
+    for {
+        ch <- "packet from 2"
+        time.Sleep(1 * time.Second)
     }
 }
-
-
-func multi4(second chan int, third chan int){
-    defer close(third)
-    for i := range second{
-        third <- i * 4
-    }
-}
-
 
 func main(){
     
-    first := make(chan int)
-    second := make(chan int)
-    third := make(chan int)
+    c1 := make(chan string)
+    c2 := make(chan string)
 
-    go producer(first)
-    go multi2(first, second)
-    go multi4(second, third)
-
-    for result := range third {
-        fmt.Println(result)
-    }
+    go goroutine1(c1)
+    go goroutine2(c2)
 }
