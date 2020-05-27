2787d3f224c08a5ea647566d98bd4382d876c0a2
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-27 18:05:08 +0900

    Update hello.go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,34 +2,25 @@ package main
  
 import (
     "fmt"
-    "time"
-    "sync"
+    
 )
 
-func goroutine(s string, wg *sync.WaitGroup ){
+func goroutine1(s []int, c chan int){
 
-    for i := 0; i < 5; i++{
-        time.Sleep(100 * time.Millisecond)
-        fmt.Println(s)
+    sum := 0
+    for _, v := range s{
+        sum += v
+        
     }
-    wg.Done()
+    c <- sum
 }
-
-func normal(s string){
-
-    for i := 0; i < 5; i++{
-        time.Sleep(100 * time.Millisecond)
-        fmt.Println(s)
-    }
-}
-
 
 func main(){
-    var wg sync.WaitGroup
-    wg.Add(1)
-    go goroutine("world", &wg)
-    normal("hello")
-    // time.Sleep(2000 * time.Millisecond)
-    wg.Wait()
     
+    s := []int{1,2,3,4,5}
+    c := make(chan int)
+    go goroutine1(s, c)
+
+    x := <-c
+    fmt.Println(x)
 }
