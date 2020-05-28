47db10c5b80df3ed8b76c7d7bf8180a6be53de8c
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-28 13:49:41 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "sort"
+    // "sort"
 	// "github.com/markcheno/go-quote"
 	// "github.com/markcheno/go-talib"
 )
@@ -15,21 +15,5 @@ const (
 
 func main() {
     
-    i := []int{1,2,3,4,5}
-    s := []string{"d", "a", "f"}
-    p := []struct{
-        Name string
-        Age int
-    }{
-        {"Nancy", 20},
-        {"Vera", 40},
-        {"Mike", 22},
-    }
-
-    fmt.Println(i, s, p)
-    sort.Ints(i)
-    fmt.Println(i)
-
-    sort.Strings(s)
-    fmt.Println(i, s)
+    fmt.Println(c1, c2, c3)
 }
