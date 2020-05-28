eb97438daaa3b48834b6827766cdae4037b685f8
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-28 13:49:14 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,29 +2,34 @@ package main
 
 import (
     "fmt"
-    "regexp"
+    "sort"
 	// "github.com/markcheno/go-quote"
 	// "github.com/markcheno/go-talib"
 )
 
-type P struct {
-    Name string
-    Age int
-}
+const (
+    c1 = iota
+    c2 
+    c3
+)
 
 func main() {
-	// spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
-	// fmt.Print(spy.CSV())
-	// rsi2 := talib.Rsi(spy.Close, 2)
-    // fmt.Println(rsi2)
     
-    match, _ := regexp.MatchString("a([a-z]+)e", "app0le")
-    fmt.Println(match)
+    i := []int{1,2,3,4,5}
+    s := []string{"d", "a", "f"}
+    p := []struct{
+        Name string
+        Age int
+    }{
+        {"Nancy", 20},
+        {"Vera", 40},
+        {"Mike", 22},
+    }
 
-    
-    r2 := regexp.MustCompile("^/(edit[save]view)/([a-zA-Z0-9]+)$")
-    fs := r2.FindString("/view/test")
-    fmt.Println(fs)
+    fmt.Println(i, s, p)
+    sort.Ints(i)
+    fmt.Println(i)
 
-    fss := r2.Find
+    sort.Strings(s)
+    fmt.Println(i, s)
 }
