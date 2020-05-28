b0ad8fff442d668a1996e5d41f01e7f823247dfd
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-28 18:35:32 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,22 +2,9 @@ package main
 
 import (
     "fmt"
+    "ioutil"
 )
-func incrementGenerator () (func() int){
 
-    x := 0
-    return func() int{
-        x++
-        return x
-    }
-}
-
-func circleArea(pi float64) func(radius float64) float64{
-
-    return func(radius float64) float64 {
-        return pi * radius * radius
-    }
-}
 
 func main() {
     // counter := incrementGenerator()
