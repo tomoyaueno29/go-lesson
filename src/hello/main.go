93c8e60d0eb5c260489cd3bf074d8cc516fd8fef
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-28 18:35:41 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -7,14 +7,5 @@ import (
 
 
 func main() {
-    // counter := incrementGenerator()
-    // fmt.Println(counter())
-    // fmt.Println(counter())
-    // fmt.Println(counter())
-
-    c1:= circleArea(3.14)
-    fmt.Println(c1(2))
-
-    c2 := circleArea(3)
-    fmt.Println(c2(2))
+    
 }
