f14e36ecde54f35ba6f9c84fdabecda8bc6f7184
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 14:28:52 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,40 +4,38 @@ import (
     "fmt"
 )
 
-// type Vertex struct {
-//     X, Y int
-// }
+type Vertex struct {
+    X, Y int
+}
 
-// func (v Vertex) Area() int{
-//     return v.X * v.Y
-// }
+func (v Vertex) Area() int {
+    return v.X * v.Y
+}
 
-// func (v *Vertex) Scale(i int) {
-//     v.X = 20
-//     v.Y = 10
-//     v.X*=i
-//     v.Y*=i
-// }
+func (v *Vertex) Scale(i int) {
+    v.X*=i
+    v.Y*=i
+}
 
-// func main() {
-//     v := new(Vertex)
-//     v.Scale(10)
-//     fmt.Println(v.Area())
-// }
+type Vertex3D struct {
+    Vertex
+    z int
+}
 
-type Person struct {
-    Name string
-    Age int
+func (v Vertex3D) Area3D() int {
+    return v.X * v.Y * v.z
+}
+
+func (v *Vertex3D) Scale(i int) {
+    v.X*=i
+    v.Y*=i
+    v.z*=i
 }
 
 func main() {
-    name := "taro"
-    namepoint := &name
+    v := Vertex{3, 4}
 
-     fmt.Printf("%v\n", namepoint)
-     fmt.Printf("%v\n", *namepoint)
-
-     *namepoint = "jiro"
-     fmt.Printf("%v\n", *namepoint)
-     fmt.Printf("%v\n", name)
+    v2 := Vertex3D{5, 6}
+    v.Scale(10)
+    fmt.Println(v.Area())
 }
