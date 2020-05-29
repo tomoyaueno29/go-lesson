7dd1fbb4a5de37b4a68f5e7d4eb7fe262023bd76
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 12:16:10 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,28 +2,14 @@ package main
 
 import (
     "fmt"
-    "log"
-    "os"
-    "io"
 )
 
-func loggingSettings(logFile string) {
-    logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-    multiLogFile := io.MultiWriter(os.Stdout, logfile)
-    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-    log.SetOutput(multiLogFile)
-}
-
 func main() {
-    _, err := os.Open("fdhgknad")
-    if err != nil{
-        log.Fatalln("exit", err)
-    }
+    var n int = 100
 
-    log.Println("logging!")
-    log.Printf("%T %v", "test", "test")
+    fmt.Println(&n)
+    fmt.Println(n)
 
-    log.Fatalf("%T %v", "test", "test")
-    log.Fatalln("error")
-    fmt.Println("ok!")
+    var p *int = &n
+    fmt.Println(*p)
 }
