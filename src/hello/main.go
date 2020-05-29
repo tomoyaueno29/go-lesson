e356ae36d88d01573a713c33c5ca4e56d3e05c08
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 12:29:45 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,12 +4,31 @@ import (
     "fmt"
 )
 
+func one(x *int) {
+    *x = 1
+}
+
 func main() {
-    var n int = 100
-
-    fmt.Println(&n)
-    fmt.Println(n)
-
-    var p *int = &n
+    /*
+    // var n int = 100
+    var p *int = new(int)
     fmt.Println(*p)
+    *p++
+    fmt.Println(*p)
+    */
+
+    s := make([]int, 0)
+    fmt.Printf("%T\n", s)
+
+    ch := make(chan int)
+    fmt.Printf("%T\n", ch)
+
+    m := make(map[string]int)
+    fmt.Printf("%T\n", m)
+
+    var p *int = new(int)
+    fmt.Printf("%T\n", p)
+
+    var st = new(struct{})
+    fmt.Printf("%T\n", st)
 }
