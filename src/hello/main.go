5727f28421d567f2c89ae1bb0694d7dd5dfb7d5a
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 13:24:24 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,21 +4,37 @@ import (
     "fmt"
 )
 
-type Vertex struct {
-    X, Y int
-}
+// type Vertex struct {
+//     X, Y int
+// }
 
-func (v Vertex) Area() int{
-    return v.X * v.Y
-}
+// func (v Vertex) Area() int{
+//     return v.X * v.Y
+// }
 
-func (v *Vertex) Scale(i int) {
-    v.X*=i
-    v.Y*=i
+// func (v *Vertex) Scale(i int) {
+//     v.X = 20
+//     v.Y = 10
+//     v.X*=i
+//     v.Y*=i
+// }
+
+// func main() {
+//     v := new(Vertex)
+//     v.Scale(10)
+//     fmt.Println(v.Area())
+// }
+
+type Person struct {
+    Name string
+    Age int
 }
 
 func main() {
-    v := &Vertex{3, 4}
-    v.Scale(10)
-    fmt.Println(v.Area())
+    p := Person{Name:"ichi", Age:17}
+    p2 := &p
+    p2.Name = "taro"
+    p2.Age = 22
+    fmt.Printf("%v\n", p)
+    fmt.Printf("%v\n", *p2)
 }
