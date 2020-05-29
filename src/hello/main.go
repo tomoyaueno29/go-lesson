66166b9fe90b6193a413adf75945f7e9645898fc
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 11:49:18 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -5,9 +5,27 @@ import (
     "time"
 )
 
+func getOsName() string {
+    return "mac"
+}
+
 func main() {
-    
+
+
+    switch os := getOsName(); os {
+    case "mac":
+        fmt.Println("mac")
+    case "windows":
+        fmt.Println("windows")
+    default:
+        fmt.Println("none")
+    }
+
     t := time.Now()
-    fmt.Println(t.Format(time.RFC3339))
-    fmt.Println(t.Year(), t.Day(), t.Hour(), t.Month())
+    fmt.Println(t.Hour())
+    switch {
+    case t.Hour() < 12:
+        
+    case t.Hour() < 17:
+    }
 }
