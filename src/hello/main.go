b37102a8ca3268b1ea47df03b5507bfb3c2c3e63
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-29 16:20:56 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,51 +4,34 @@ import (
     "fmt"
 )
 
-type Income interface {
-    calculate() string
+type Human interface {
+    Say() string
 }
 
-type google struct {
-    workingYear, baseSalary, performance int
+type Person struct {
+    Name string
 }
 
-type yahoo struct {
-    performance, workingYear, baseSalary int
+type Dog string {
+    Name string
 }
 
-func (g google) calculate() int {
-    return g.baseSalary + (1000 * g.performance)
+func (p *Person) Say()  string{
+    p.Name = "Mr." + p.Name
+    fmt.Println(p.Name)
+    return p.Name
 }
 
-func (y yahoo) calculate() int {
-    return y.baseSalary + (20000 * y.workingYear)
+func Driver(human Human){
+    if human.Say() == "Mr.Mike"{
+        fmt.Println("Run")
+    }else{
+        fmt.Println("Get out")
+    }
 }
 
 func main() {
 
-    hanako := google{
-        workingYear: 5,
-        baseSalary: 100000,
-        performance: 190,
-    }
-    choko := yahoo{
-        baseSalary: 60000,
-        workingYear: 50,
-    }
-    motoko := yahoo{
-        baseSalary: 40000,
-        workingYear: 25,
-    }
-
-    workers := []Income{hanako, choko, motoko}
-    fmt.Printf("Total income: %d\n", calculateIncome(workers))
-}
-
-func calculateIncome(ic []Income) int {
-    sum := 0
-    for _, worker := range ic{
-        sum += worker.calculate()
-    }
-
-    return sum
+    var mike Human = &Person{"Mike"}
+    Driver(mike)
 }
