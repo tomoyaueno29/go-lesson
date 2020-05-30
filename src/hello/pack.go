54008a322d8ada376d25a11efc22592a4450ec3d
tomoyaueno29 <u.t012807@gmail.com>
2020-05-30 20:53:52 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	// "io/ioutil"
+	// "net/http"
+	"net/url"
+	"log"
 )
 
 func main() {
-	resp, _ := http.Get("http://example.com")
-	defer resp.Body.Close()
+	// resp, _ := http.Get("http://example.com")
+	// defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+
+	base, err := url.Parse("http://example.com")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(base)
+
+	reference, _ := url.Parse("/test?a=1&b=2")
+
+	end
 }
