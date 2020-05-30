1f38e5b86ee32fdb2ff46cebc38d4c751b8550bd
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 11:19:06 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,45 +1,14 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
 )
 
-type Vertex struct {
-    X, Y int
-}
-
-func (v Vertex) Area() int {
-    return v.X * v.Y
-}
-
-func (v *Vertex) Scale(i int) {
-    v.X = v.X*i
-    v.Y = v.Y*i
-}
-func New(x, y int) *Vertex {
-    return &Vertex{x, y}
-}
-
-type Vertex3D struct {
-    Vertex
-    Z int
-}
-
-func (v Vertex3D) Area3D() int {
-    return v.X * v.Y * v.Z
-}
-
-func (v *Vertex3D) Scale3D(i int) {
-    v.X = v.X*i
-    v.Y = v.Y*i
-    v.Z = v.Z*i
-}
-
-
 func main() {
-    // v := New(3, 4)
-    // v := &Vertex{X:3, Y:4}
-    v := &Vertex3D{Vertex{3,4}, 5}
-    v.Scale3D(10)
-    fmt.Println(v.Area3D())
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
