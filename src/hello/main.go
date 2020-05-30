6b318454c03fb922f2a4fa70677f22755c6e5c62
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 17:37:51 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,20 +3,33 @@ package main
 import (
     "fmt"
     "encoding/json"
+    "log"
+    "time"
 )
 
 type User struct {
     Id int
     Name string
     Email string
+    Create time.Time
 }
 
 func main() {
-    u := new(User)
-    u.Id = 1
-    u.Name = "tomoya"
-    u.Email = "xxx@a.com"
+    src := `
+    {
+    "Id":1,
+    "Name":"tomoya",
+    "Email":"aa@a.com",
+    "time":"2020/4532928170223"
+    }
+    `
 
-    bs, _ := json.Marshal(u)
-    fmt.Println(string(bs))
+    u := new(User)
+
+    err := json.Unmarshal([]byte(src), u)
+    if err != nil{
+        log.Fatal(err)
+    }
+
+    fmt.Println(u)
 }
