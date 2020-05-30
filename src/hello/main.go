1d9ff17274d1de9a84100375f76caa3ed55a1835
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 12:47:49 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -10,7 +10,11 @@ func main() {
 
     content, err := ioutil.ReadFile("main.go")
     if err != nil {
-        log.Fatal(err)
+        log.Fatalln(err)
     }
     fmt.Println(string(content))
+
+    if err := ioutil.WriteFile("ioutil temp.go", content, 0666); err != nil{
+        log.Fatalln(err)
+    }
 }
