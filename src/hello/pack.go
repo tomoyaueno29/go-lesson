0691ab45768e90ef93086126c5ea4178ae5d35a8
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 19:38:30 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"io/ioutil"
 )
 
 func main(){
-	dir,_ := ioutil.TempDir("", "sa")
-	fmt.Println(dir)
-	defer os.RemoveAll(dir)
+	content, err := ioutil.ReadFile("pack.go")
+	if err != nil{
+		log.Fatalln(err)
+	}
+	fmt.Println(string(content))
+
+	if err := ioutil.WriteFile("ioutil_temp.go", content, 0666); err != nil{
+		log.Fatalln(err)
+	}
+
+
 }
