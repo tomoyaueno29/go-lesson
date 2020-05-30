774a323e9345d7856a0cda6b9e9661a166d38bca
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 17:33:41 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-    "bytes"
     "fmt"
-    "io/ioutil"
-    // "log"
+    "encoding/json"
 )
 
+type User struct {
+    Id int
+    Name string
+    Email string
+}
+
 func main() {
+    u := new(User)
+    u.Id = 1
+    u.Name = "tomoya"
+    u.Email = "xxx@a.com"
 
-    // content, err := ioutil.ReadFile("main.go")
-    // if err != nil {
-    //     log.Fatalln(err)
-    // }
-    // fmt.Println(string(content))
-
-    // if err := ioutil.WriteFile("ioutil temp.go", content, 0666); err != nil{
-    //     log.Fatalln(err)
-    // }
-
-    r := bytes.NewBuffer([]byte("abc"))
-    content, _ := ioutil.ReadAll(r)
-
-    fmt.Println(string(content))
+    bs, _ := json.Marshal(u)
+    fmt.Println(string(bs))
 }
