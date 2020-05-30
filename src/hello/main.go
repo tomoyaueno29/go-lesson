fcbca3fa3e16ab969413b88b64003d260edf2933
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 17:44:37 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,7 +4,7 @@ import (
     "fmt"
     "encoding/json"
     "log"
-    "time"
+    // "time"
 )
 
 type User struct {
@@ -14,7 +14,14 @@ type User struct {
 }
 
 func main() {
-    b := []byte(`{"Id":1, "Name":"tomoya", "Email":"a@a.com"}`)
+    b := []byte(`{"id":1, "name":"tomoya", "email":"a@a.com"}`)
+    p := new(User)
 
-    
+    if err := json.Unmarshal(b, &p); err != nil{
+        log.Fatal(err)
+    }
+    fmt.Println(p.Id, p.Name, p.Email)
+
+    v, _ := json.Marshal(p)
+    fmt.Println(string(v))
 }
