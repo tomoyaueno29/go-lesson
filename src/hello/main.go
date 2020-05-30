ece59423351525d9ebd993d29bdc9464853ed30d
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 18:19:18 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,41 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "encoding/json"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
     "log"
-    // "time"
 )
 
-type T struct{
+var DbConnection *sql.DB
 
-}
-
-type User struct {
-    Id    int       `json:"id,string"`
-    Name  string    `json:"name,omitempty"`
-    Email string    `json:"email"`
-    T     *T         `json:"T,omitempty"`
-}
-
-func (u User) MarshalJSON() ([]byte, error) {
-
-    v, _ := json.Marshal(&struct{
-        Name string
-    }{
-        
-    })
-}
-
-func main() {
-    b := []byte(`{"id":"1", "name":"", "email":"a@a.com"}`)
-    p := new(User)
-
-    if err := json.Unmarshal(b, &p); err != nil{
+func main(){
+    DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+    defer DbConnection.Close()
+    cmd := `CREATE TABLE IF NOT EXISTS person(
+                name String,
+                age INT)`
+    _, err := DbConnection.Exec(cmd)
+    if err != nil{
         log.Fatal(err)
     }
-    fmt.Println(p.Id, p.Name, p.Email)
 
-    v, _ := json.Marshal(p)
-    fmt.Println(string(v))
+    cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
+    _, err = DbConnection.Exec(cmd, "Mike", 24)
+    if err != nil{
+        log.Fatalln(err)
+    }
 }
