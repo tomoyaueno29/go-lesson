1b110b59d76f7458dc875347388440c31b690be2
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 17:40:42 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -11,25 +11,10 @@ type User struct {
     Id int
     Name string
     Email string
-    Create time.Time
 }
 
 func main() {
-    src := `
-    {
-    "Id":1,
-    "Name":"tomoya",
-    "Email":"aa@a.com",
-    "time":"2020/4532928170223"
-    }
-    `
+    b := []byte(`{"Id":1, "Name":"tomoya", "Email":"a@a.com"}`)
 
-    u := new(User)
-
-    err := json.Unmarshal([]byte(src), u)
-    if err != nil{
-        log.Fatal(err)
-    }
-
-    fmt.Println(u)
+    
 }
