86b6e4579c69da72e48cc979c4e0bec0900615f1
tomoyaueno29 <u.t012807@gmail.com>
2020-05-30 20:35:30 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -8,17 +8,17 @@ import (
 )
 
 func main() {
-	// 読み込み
-	content, err := ioutil.ReadFile("pack.go")
-	if err != nil{
-		log.Fatalln(err)
-	}
-	fmt.Println(string(content))
-	// buf := make([]byte, 100)
+	// // 読み込み
+	// content, err := ioutil.ReadFile("pack.go")
+	// if err != nil{
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(string(content))
+	// // buf := make([]byte, 100)
 
-	if err := ioutil.WriteFile("ioutil_temp.go", content, 0666); err != nil{
-		log.Fatalln(err)
-	}
+	// if err := ioutil.WriteFile("ioutil_temp.go", content, 0666); err != nil{
+	// 	log.Fatalln(err)
+	// }
 
 
 }
