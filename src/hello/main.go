956fa6efa4e1ab9e8caec7ddeeb18753e20b1613
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-30 12:22:07 +0900

    Update main.go

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-    "context"
     "fmt"
-    "time"
+    "io/ioutil"
 )
-
-func longProcess(ctx context.Context, ch chan string) {
-    fmt.Println("run")
-    time.Sleep(2*time.Second)
-    fmt.Println("finish")
-    ch <- "result"
-}
-
 
 func main() {
 
-    ch := make(chan string)
-    ctx := context.Background()
-    ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
-    defer cancel()
-    go longProcess(ctx, ch)
-
-    CTXLOOP:
-    for {
-        select {
-        case <- ctx.Done():
-            fmt.Println(ctx.Err())
-            break CTXLOOP
-
-        case <- ch:
-            fmt.Println("seccess")
-            break CTXLOOP
-        }
-    }
-    fmt.Println("#####################")
+    content, err := ioutil.ReadFile("main.go")
 }
