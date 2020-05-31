a7c0dc312b37ca50d09b12940f874c3d4620a03b
tomoyaueno29 <u.t012807@gmail.com>
2020-05-31 19:01:31 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -12,17 +12,13 @@ type Person struct {
 	Age int
 }
 
-func one(x int) {
-	x = 1
+func one(x *int) {
+	*x = 1
 }
 
 func main() {
 
 	var n int = 100
+	one(&n)
 	fmt.Println(n)
-	fmt.Println(&n)
-
-	var p *int = &n
-	fmt.Println(p)
-	fmt.Println(*p)
 }
