36806dc17f0b306ea660a3cf08102f794172d858
tomoyaueno29 <u.t012807@gmail.com>
2020-05-31 19:16:35 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	// "encoding/json"
-	// "io/ioutil"
-	// "log"
+	"net/http"
+	"io/ioutil"
 )
 
-type Person struct {
-	Name string
-	Age int
-}
-
 func main() {
-    var i int = 100
-    var j int = 200
-    var p1 *int
-    var p2 *int
-    p1 = &i
-    p2 = &j
-    i = *p1 + *p2
-	p2 = p1
-	fmt.Println(*p2)
-    j = *p2 + i
-    fmt.Println(j)
+	res, _ := http.Get("http://example.com")
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
 }
