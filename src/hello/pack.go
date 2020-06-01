45cefb456daa7805d449c2d6af3603a683047181
tomoyaueno29 <u.t012807@gmail.com>
2020-06-01 12:32:22 +0900

    Update pack.go

--- src/hello/pack.go
+++ src/hello/pack.go
@@ -2,33 +2,21 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/url"
-	"io/ioutil"
+	"encoding/json"
+	// "io/ioutil"
 )
 
+type Person struct {
+	Name string	`json:"name"`
+	Age int	`json:"age"`
+	Nicknames []string	`json:"nicknames"`
+}
+
 func main() {
-	// resp, _ := http.Get("http://example.com")
-	// defer resp.Body.Close()
-
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println(body)
-	// fmt.Println(string(body))
-
-	base, _ := url.Parse("http://example.com/")
-	reference, _ := url.Parse("/test?a=1&b=2")
-	endpoint := base.ResolveReference(reference).String()
-	fmt.Println(endpoint)
-
-	req, _ := http.NewRequest("GET", endpoint, nil)
-	req.Header.Add("acabckava", "Wcdcnalc")
-	q := req.URL.Query()
-	q.Add("c", "3")
-	req.URL.RawQuery = q.Encode()
-	fmt.Println(q.Encode())
-
-	var client *http.Client = &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	b := []byte(`{"name":"tomoya", "age":22, "nicknames":["a", "b", "c"]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
 }
